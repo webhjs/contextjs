a4607c90693cf04400d774410fb29667f526e404
webhjs <1395516797@qq.com>
2019-07-24 12:40:19 +0800

    first add menu

---
+++ menu.vue
@@ -0,0 +1,142 @@
+<template>
+    <transition name="fade" mode="out-in">
+        <div id="menu_wrapper" @contextmenu.prevent="" ref='userContain' v-show="visble">
+            <slot></slot>
+        </div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+               firstInit:[]
+            }
+        },
+        props:{
+            page:{
+                type:Object,
+                default(){
+                    return {
+                        pageX:0,
+                        pageY:0
+                    }
+                }
+            },
+            visble:{
+                type:Boolean,
+                default:false
+            }
+        },
+        watch: {
+            page:{
+                handler(val,old){
+                    this.$nextTick(()=>{
+                        this.computedPosition(val)
+                    })
+                },
+                deep:true
+            }
+        },
+        methods:{
+            computedPosition(pos){
+                let doc = document.getElementById('menu_wrapper'),
+                    h = parseInt(window.getComputedStyle(doc).height),
+                    w = parseInt(window.getComputedStyle(doc).width),
+                    // ch = window.screen.availHeight,
+                    // cw = window.screen.availWidth,
+                    dh = document.documentElement.clientHeight,
+                    dw = document.documentElement.clientWidth,
+                    dzoom = this.detectZoom()/100
+                if((pos.pageX+w) > dw){
+                    doc.style.left = (pos.pageX-w)+'px';
+                }else{
+                    doc.style.left = pos.pageX+'px';
+                }
+                if((pos.pageY+h) > dh){
+                    doc.style.top = (pos.pageY-h)+'px';
+                }else{
+                    doc.style.top = pos.pageY+'px';
+                }
+                this.firstInit = [parseInt(doc.style.top),parseInt(doc.style.left),dzoom]
+            },
+            comPagePos(){
+                let doc = document.getElementById('menu_wrapper'),
+                    dzoom = this.detectZoom()/100
+                    doc.style.top = this.firstInit[0]/dzoom*this.firstInit[2] +'px'
+                    doc.style.left = this.firstInit[1]/dzoom*this.firstInit[2] +'px'
+                this.$nextTick(()=>{
+                    let docs = document.getElementById('menu_wrapper'),
+                        h = parseInt(window.getComputedStyle(docs).height), 
+                        w = parseInt(window.getComputedStyle(docs).width),
+                        dh = document.documentElement.clientHeight,
+                        dw = document.documentElement.clientWidth;
+                    if((parseInt(docs.style.left)+w) > dw){
+                        docs.style.left = (parseInt(docs.style.left)-w)+'px';
+                        console.log('docs.style.left')
+                    }
+                    if((parseInt(docs.style.top)+h) > dh){
+                        docs.style.top = (parseInt(docs.style.top)-h)+'px';
+                    }
+                })
+            },
+            detectZoom() {    //缩放比列
+                let ratio = 0,
+                    screen = window.screen,
+                    ua = navigator.userAgent.toLowerCase();
+                
+                if (window.devicePixelRatio !== undefined) {
+                    ratio = window.devicePixelRatio;
+                }
+                else if (~ua.indexOf('msie')) {
+                    if (screen.deviceXDPI && screen.logicalXDPI) {
+                        ratio = screen.deviceXDPI / screen.logicalXDPI;
+                    }
+                }
+                else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
+                    ratio = window.outerWidth / window.innerWidth;
+                }
+                
+                if (ratio) {
+                    ratio = Math.round(ratio * 100);
+                }
+                return ratio;
+            }
+        },
+        mounted(){
+            document.addEventListener('click',(e)=>{
+                this.$nextTick(()=>{
+                    if(!!this.$refs.userContain.contains(e.target)) return
+                    this.$emit('update:visble', false) 
+                })
+            })
+            let timer = null;
+            window.onresize = () => { 
+                if(timer){
+                    clearInterval(timer);
+                }
+                timer = setTimeout(this.comPagePos(),300);
+            };
+            window.onscroll = () => { 
+                if(timer){
+                    clearInterval(timer);
+                }
+                timer = setTimeout(this.comPagePos(),300);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #menu_wrapper{
+        display: inline;
+        position: fixed;
+    }
+    .fade-enter-active, .fade-leave-active {
+        transition:opacity .6s ease-out;
+    }
+    .fade-enter, .fade-leave-to{
+        opacity:0;
+    }
+</style>
+
